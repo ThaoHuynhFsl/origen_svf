3302910c2454154a13f2b381028efc99b2a1c662
Thao Huynh <thao.huynh@nxp.com>
2022-03-24 08:38:08 -0700

    remove debugger statement

--- lib/origen_svf/tester.rb
+++ lib/origen_svf/tester.rb
@@ -104,60 +104,61 @@ module OrigenSVF
         super
       end
     end
+
     def handshake(options = {})
       ss 'Tester handshake is not support.  Add comment here to highlight'
     end
 
     def start_subroutine(name)
-        local_subroutines << name.to_s.chomp unless local_subroutines.include?(name.to_s.chomp) || @inhibit_vectors
-        # name += "_subr" unless name =~ /sub/
-        ::Pattern.open name: name, call_startup_callbacks: false, subroutine: true
-      end
+      local_subroutines << name.to_s.chomp unless local_subroutines.include?(name.to_s.chomp) || @inhibit_vectors
+      # name += "_subr" unless name =~ /sub/
+      ::Pattern.open name: name, call_startup_callbacks: false, subroutine: true
+    end
 
-      # Ends the current subroutine that was started with a previous call to start_subroutine
-      def end_subroutine(_cond = false)
-        ::Pattern.close call_shutdown_callbacks: false, subroutine: true
-      end
-# Returns an array of subroutines created by the current pattern
-      def local_subroutines # :nodoc:
-        @local_subroutines ||= []
-      end
+    # Ends the current subroutine that was started with a previous call to start_subroutine
+    def end_subroutine(_cond = false)
+      ::Pattern.close call_shutdown_callbacks: false, subroutine: true
+    end
+    # Returns an array of subroutines created by the current pattern
+    def local_subroutines # :nodoc:
+      @local_subroutines ||= []
+    end
 
-      def loop_vectors(name = nil, number_of_loops = 1, _global = false)
-        # The name argument is present to maych J750 API, sort out the
-        unless name.is_a?(String)
-          name, number_of_loops, global = nil, name, number_of_loops
-        end
-        if number_of_loops > 1
-          cc "Looping is not support by SVF.  Add comment here to highligh."
-          cc "LOOPING #{name} #{number_of_loops} times"
-          yield
-          cc "END LOOPING #{name}"
-        else
-          yield
-        end
+    def loop_vectors(name = nil, number_of_loops = 1, _global = false)
+      # The name argument is present to maych J750 API, sort out the
+      unless name.is_a?(String)
+        name, number_of_loops, global = nil, name, number_of_loops
       end
-      alias_method :loop_vector, :loop_vectors
+      if number_of_loops > 1
+        cc 'Looping is not support by SVF.  Add comment here to highligh.'
+        cc "LOOPING #{name} #{number_of_loops} times"
+        yield
+        cc "END LOOPING #{name}"
+      else
+        yield
+      end
+    end
+    alias_method :loop_vector, :loop_vectors
 
-      def match_block(timeout_in_cycles, options = {}, &block)
-          cc "Matchloop is not support by SVF.  Add comment here to highligh."
-          cc "Matchloop is waiting for #{timeout_in_cycles} cycles "
-      end
+    def match_block(timeout_in_cycles, options = {}, &block)
+      cc 'Matchloop is not support by SVF.  Add comment here to highligh.'
+      cc "Matchloop is waiting for #{timeout_in_cycles} cycles "
+    end
 
-      def match(pin, state, timeout_in_cycles, options = {})
-          cc "Matchloop is not support by SVF.  Add comment here to highligh."
-          cc "Matchloop is waiting on pin #{pin} to go #{state}"
-          cc "Matchloop is waiting for #{timeout_in_cycles} cycles "
-      end
+    def match(pin, state, timeout_in_cycles, options = {})
+      cc 'Matchloop is not support by SVF.  Add comment here to highligh.'
+      cc "Matchloop is waiting on pin #{pin} to go #{state}"
+      cc "Matchloop is waiting for #{timeout_in_cycles} cycles "
+    end
 
     private
 
     def data(reg_or_val, options = {})
-      unless reg_or_val.is_a? Numeric
-      if reg_or_val.has_overlay?
-        debugger
-      end
-      end
+      # unless reg_or_val.is_a? Numeric
+      # if reg_or_val.has_overlay?
+      #  debugger
+      # end
+      # end
       d = reg_or_val.respond_to?(:data) ? reg_or_val.data : reg_or_val
       d.to_s(16).upcase
     end
@@ -180,34 +181,32 @@ module OrigenSVF
     end
 
     def s_to_cycles(time) # :nodoc:
-        ((time.to_f) * 1000 * 1000 * 1000 / current_period_in_ns).to_int
-      end
+      ((time.to_f) * 1000 * 1000 * 1000 / current_period_in_ns).to_int
+    end
 
-      def ms_to_cycles(time) # :nodoc:
-        ((time.to_f) * 1000 * 1000 / current_period_in_ns).to_int
-      end
+    def ms_to_cycles(time) # :nodoc:
+      ((time.to_f) * 1000 * 1000 / current_period_in_ns).to_int
+    end
 
-      def us_to_cycles(time) # :nodoc:
-        ((time.to_f * 1000) / current_period_in_ns).to_int
-      end
+    def us_to_cycles(time) # :nodoc:
+      ((time.to_f * 1000) / current_period_in_ns).to_int
+    end
 
-      def ns_to_cycles(time) # :nodoc:
-        (time.to_f / current_period_in_ns).to_int
-      end
+    def ns_to_cycles(time) # :nodoc:
+      (time.to_f / current_period_in_ns).to_int
+    end
 
-      def cycles_to_us(cycles) # :nodoc:
-        ((cycles.to_f * current_period_in_ns) / (1000)).ceil
-      end
+    def cycles_to_us(cycles) # :nodoc:
+      ((cycles.to_f * current_period_in_ns) / (1000)).ceil
+    end
 
-      def cycles_to_ms(cycles) # :nodoc:
-        ((cycles.to_f * current_period_in_ns) / (1000 * 1000)).ceil
-      end
+    def cycles_to_ms(cycles) # :nodoc:
+      ((cycles.to_f * current_period_in_ns) / (1000 * 1000)).ceil
+    end
 
-      # Cycles to tenths of a second
-      def cycles_to_ts(cycles) # :nodoc:
-        ((cycles.to_f * current_period_in_ns) / (1000 * 1000 * 100)).ceil
-      end
-
-
+    # Cycles to tenths of a second
+    def cycles_to_ts(cycles) # :nodoc:
+      ((cycles.to_f * current_period_in_ns) / (1000 * 1000 * 100)).ceil
+    end
   end
 end
